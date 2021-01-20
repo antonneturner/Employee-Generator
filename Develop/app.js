dc95e00265620bb75ea2d9aa65994f5f8bf1dc44
Antonne Turner <antonneturner@gmail.com>
2021-01-20 14:16:17 -0500

    add if statement for manager, else if statment for engineer and inter, create else statement for html file to render to employee list, and console log success

--- Develop/app.js
+++ Develop/app.js
@@ -9,10 +9,12 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Choices = require("inquirer/lib/objects/choices");
 
 
 let questions = []
 
+let employeeList = []
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -23,27 +25,93 @@ function askQuestions() {
         {
             type: "input",
             message: "what is your name?",
-            name: "name"
+            name: "fullname"
 
         },
         {
             type: "input",
             message: "what is your id?",
-            name: "name"
+            name: "id"
 
         },
+        {
+            type: "input",
+            message: "what is your email?",
+            name: "email"
+
+        },
+        {
+            type: "list",
+            message: "what type of employee do you want to add?",
+            name: "type",
+            choices: ["manager", "intern", "engineer", "exit"]
+
+        }
+
+    ];
+
+    let manager = {
+        type: "input",
+        message: "what is your officeNumber?",
+        name: "officenumber"
+    }
+
+    let engineer = {
+        type: "input",
+        message: "what is your github?",
+        name: "github"
+    }
+
+    let intern = {
+        type: "input",
+        message: "what school do you go to?",
+        name: "school"
+    }
 
 
-    ]
 
-    let manager = [{}]
+    inquirer.prompt(questions).then(function (response) {
 
-    inquirer.prompt(questions).then(function () {
+        if (response.type === "manager") {
+            inquirer.prompt(manager).then(function (managerResponse) {
+                const newManager = new Manager(response.fullname, response.id, response.email, managerResponse.officenumber)
+                employeeList.push(newManager)
+                askQuestions()
+            })
+        }
+        else if (response.type === "engineer") {
+            inquirer.prompt(engineer).then(function (engineerResponse) {
+                const newEngineer = new Engineer(response.fullname, response.id, response.email, engineerResponse.github)
+                employeeList.push(newEngineer)
+                askQuestions()
+            })
+        }
+        else if (response.type === "intern") {
+            inquirer.prompt(intern).then(function (internResponse) {
+                const newIntern = new Intern(response.fullname, response.id, response.email, internResponse.school)
+                employeeList.push(newIntern)
+                askQuestions()
+            })
+        }
+        else {
+            var htmlContent = render(employeeList)
+            fs.writeFile(outputPath, htmlContent, function (error) {
+                if (error) {
+                    throw error
+                }
 
+                console.log("success")
+            })
+        }
 
     })
 }
 
+
+
+
+
+askQuestions()
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
